9f4ce6bea5b76b6e09ae193e103bd18fb1c7e315
Harshit Singh <54538211+HarshitSingh27@users.noreply.github.com>
2021-10-10 21:16:15 +0530

    Edit Distance

---
+++ 6dynamic.cpp
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int editDistance(string s, string t) {
+    int n = s.size(), m = t.size(), dp[n + 1][m + 1];
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            if (i == 0 || j == 0)dp[i][j] = i ^ j;
+            else if (s[i - 1] == t[j - 1])dp[i][j] = dp[i - 1][j - 1];
+            else dp[i][j] = 1 + min({dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]});
+        }
+    }
+    return dp[n][m];
+} 
+};
+
+// { Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s, t;
+        cin >> s >> t;
+        Solution ob;
+        int ans = ob.editDistance(s, t);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
