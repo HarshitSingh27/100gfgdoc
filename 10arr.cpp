6839ae55ae0e980a548f7cdab57935169607b176
Harshit Singh <54538211+HarshitSingh27@users.noreply.github.com>
2021-10-14 21:53:28 +0530

    Valid Pair Sum

---
+++ 10arr.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+class Solution{
+    
+    public:
+    long long ValidPair(int a[], int n) 
+    {
+        sort(a,a+n);
+        int i=0,j=n-1;
+        long long count=0;
+        while(i!=j)
+        {
+            if(a[i]+a[j]>0)
+            {
+                count+=j-i; //Since all values on the right will also have sum>0
+                j--;
+            }
+            else if(a[i]+a[j]<=0)
+            {
+                i++;
+            }
+        }
+        return count;
+    } 
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int array[n];
+		for (int i = 0; i < n; ++i)
+			cin>>array[i];
+	    Solution obj;
+		cout << obj.ValidPair(array, n) <<"\n";
+	}
+	return 0; 
+} 
+
+  // } Driver Code Ends
