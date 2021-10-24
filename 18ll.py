6534cf9521fbff3afb887685a497e3ac3522dbd6
Harshit Singh <54538211+HarshitSingh27@users.noreply.github.com>
2021-10-24 23:09:01 +0530

    Segregate even and odd nodes

---
+++ 18ll.py
@@ -0,0 +1,103 @@
+# User function Template for Python3
+
+# Following is the structure of node 
+# class Node:
+#     def __init__(self):  
+#         self.data = None
+#         self.next = None
+
+class Solution:
+    def divide(self, N, head):
+       # code here
+       
+       even = None
+       odd = None
+       evenStart = None
+       oddStart = None
+       
+       ptr = head
+       
+       while(ptr):
+           if (ptr.data %2 == 0):
+               if (even == None):
+                   even = ptr
+                   evenStart = ptr
+               else:
+                   even.next = ptr
+                   even = ptr
+           
+           else:
+               if (odd == None):
+                   odd = ptr
+                   oddStart = ptr
+                   
+               else:
+                   odd.next = ptr
+                   odd = ptr
+           
+           ptr = ptr.next
+       
+       if (evenStart == None):
+           odd.next = None
+           return oddStart
+           
+       if (oddStart == None):
+           even.next = None
+           return evenStart
+           
+       even.next = oddStart
+       odd.next = None
+       
+       return evenStart
+        # code here
+
+#{ 
+#  Driver Code Starts
+# Initial Template for Python3
+
+# Node Class    
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def insert(self, data):
+        if self.head == None:
+            self.head = node()
+            self.tail = self.head
+            self.head.data = data
+        else:
+            new_node = node()
+            new_node.data = data
+            new_node.next = None
+            self.tail.next = new_node
+            self.tail = self.tail.next
+
+def printlist(head):
+    temp = head
+    while temp is not None:
+        print(temp.data, end=" ")
+        temp = temp.next
+    print('')
+
+# Driver Program
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        list1 = Linked_List()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in values:
+            list1.insert(i)
+        ob = Solution()
+        newhead = ob.divide(n, list1.head)
+        printlist(newhead)
+
+
+# } Driver Code Ends
